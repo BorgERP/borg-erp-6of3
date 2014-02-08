cc2522c181fbd5caa48f2967ddfb5023a34cfaed
goran <goran@MSI-GX720.(none)>
2014-02-08 03:30:53 +0100

    Folder proposal

--- l10n_hr/l10n_hr_bank_fina_mn/report/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import parser_account_bank_statement
-import parser_account_bank_statement_move
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
--- l10n_hr/l10n_hr_bank_fina_mn/wizard/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-import fina_mn_import
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
